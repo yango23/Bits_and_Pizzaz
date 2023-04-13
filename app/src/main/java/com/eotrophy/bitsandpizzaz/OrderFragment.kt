9e32595fff2961df5733b33eeb9902a2dc388ead
Gleb Kabyshev <gleb.kabyshev@playrix.com>
2023-04-14 03:44:48 +0400

    fab, radiobuttons, and chip was add

--- app/src/main/java/com/eotrophy/bitsandpizzaz/OrderFragment.kt
+++ app/src/main/java/com/eotrophy/bitsandpizzaz/OrderFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -19,6 +20,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class OrderFragment : Fragment() {
+
+//    val pizzaGroup = view?.findViewById<RadioGroup>(R.id.radio_group)
+//    val pizzaType = pizzaGroup.checkedRadioButtonId
+//    if (pizza)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
